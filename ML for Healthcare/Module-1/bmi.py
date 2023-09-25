3668e92b43cb8200ae5f6c0d0c35a5eb2ddedbe6
Sai Nishwanth <sainishwanthraj@gmail.com>
2023-09-25 09:47:25 +0530

    25-09-2023

---
+++ ML for Healthcare/Module-1/bmi.py
@@ -0,0 +1,3 @@
+mass = int(input("Enter weight in KG: "))
+height = float(input("Enter height in ms: "))
+print(f"BMI - {mass/pow(height,2)}")
---
+++ Web Technology/Module 3/variables.js
@@ -0,0 +1,3 @@
+const PI = 3.141592653589793;
+let str = "Hello";
+console.log(str.slice(0,2));
